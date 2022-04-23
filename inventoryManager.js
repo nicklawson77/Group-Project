3e605a993674d9306cd7d5bf1a777e650bf8d0aa
Your Name <you@example.com>
2022-04-22 17:53:57 -0700

    second commit

--- inventoryManager.js
+++ inventoryManager.js
@@ -1,13 +1,76 @@
 class Item {
     constructor(name) {
         this.name = name;
-        this.qty = [];
+        this.discriptions = [];
+    }
+    
+    addItem(price, qty, amountSold) {
+        this.items.push(new Item(price, qty, amountSold));
     }
 }
 
-class Qty {
-    constructor(amountOf){
-        this.amountOf = amountOf;
+class Discripton {
+    constructor(qty){
+        this.qty = qty;
+        this.price = price;
+        this.amountSold = amountSold;
     }
 }
 
+class ItemService {
+    static url = "http://crudcrud.com/apid4cd6184ffd74f848e28ac8657748f1b";
+
+    static getAllItems() {
+        return $.get(this.url);
+    }
+
+    static getItem(id) {
+        return $.get(this.url + `/${id}`);
+    }
+
+    static createItem(item) {
+        return $.post(this.url, item);
+    }
+
+    static updateItem(item) {
+        return $.ajax({
+            url: this.url + `/${item._id}`,
+            dataType: 'json',
+            data: JSON.stringify(item),
+            contentType: `application/json`,
+            type: 'PUT'
+        });
+    }
+
+    static deleteItem(id) {
+        return $.ajax({
+            url: thislurl + `/${id}`,
+            type: "DELETE"
+        })
+    }
+}
+
+class DOMManager {
+    static items;
+
+    static getAllItems() {
+        ItemService.getAllItems().then(items => this.render(items));
+    }
+
+    static render(items) {
+      this.items = items;
+      $(`#app`).empty();
+      for (let item of items) {
+          $(`app`).prepend(
+              `<div id="${item._id}" class="card>
+                <div class ="card-header">
+                <h2>${item.name}</h2>
+                </div>
+                
+                `
+          );
+      }  
+    }
+}
+
+DOMManager.getAllItems()
